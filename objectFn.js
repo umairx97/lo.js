ade198c0ef6445be85b281505894b5877fed3417
umairx97 <umair.bajwa@hotmail.com>
2020-07-31 19:59:13 +0500

    doc(filterKeys): add parameter definition documentation

--- objectFn.js
+++ objectFn.js
@@ -1,7 +1,11 @@
 module.exports = {
   filterKeys
 }
-
+/**
+ * @param  {Object} obj - plain js object which keys you want to map through
+ * @param  {Array} keys - which keys you want to return in resulting object (only high level object keys will work)
+ * @returns {Object}
+ */
 function filterKeys (obj = {}, keys = []) {
   const cloned = { ...obj }
   return keys.reduce((acc, item) => {
