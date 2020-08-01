051b506395a3328d8b63816c3c9cb7434a23f030
umairx97 <umair.bajwa@hotmail.com>
2020-08-01 19:34:33 +0500

    feat: add isArray and includesAll in booleanFn

--- booleanFn.js
+++ booleanFn.js
@@ -1,6 +1,8 @@
 module.exports = {
   isOdd,
-  isEven
+  isEven,
+  includesAll,
+  isArray
 }
 
 /**
@@ -15,4 +17,19 @@ function isOdd (num) {
  */
 function isEven (num) {
   return num % 2 === 0
+}
+
+/**
+ * @param {Array} arr - JS Array
+ */
+function isArray (arr) {
+  return Array.isArray(arr)
+}
+
+/**
+ * @param {Array} arr - array to check from if all values exist
+ * @param {Array} values - array of values to check
+ */
+function includesAll (arr = [], values = []) {
+  return values.every(val => arr.includes(val))
 }
--- tests/booleanFn.test.js
+++ tests/booleanFn.test.js
@@ -1,5 +1,5 @@
 const tape = require('tape')
-const { isOdd, isEven } = require('../booleanFn')
+const { isOdd, isEven, includesAll, isArray } = require('../booleanFn')
 
 tape('Fn:(isOdd)', function (suite) {
   tape('ideal parameters should return expected data', function (t) {
@@ -18,6 +18,28 @@ tape('Fn:(isEven)', function (suite) {
     const testNumbers = [1, 2, 3, 500, 201]
 
     testNumbers.forEach((num, idx) => t.equal(isEven(num), expectedOutputs[idx], 'should be equal to expected result'))
+    t.end()
+  })
+  suite.end()
+})
+
+tape('Fn:(includesAll)', function (suite) {
+  tape('ideal parameters should return expected data', function (t) {
+    const expectedOutput = true
+    const testNumbers = [1, 2, 3, 500, 201]
+
+    t.equal(includesAll(testNumbers, testNumbers), expectedOutput)
+    t.end()
+  })
+  suite.end()
+})
+
+tape('Fn:(includesAll)', function (suite) {
+  tape('ideal parameters should return expected data', function (t) {
+    const expectedOutput = true
+    const testData = []
+
+    t.equal(isArray(testData), expectedOutput)
     t.end()
   })
   suite.end()
